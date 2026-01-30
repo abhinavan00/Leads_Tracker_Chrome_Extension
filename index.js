098ae5f464ec99012d84d263b9daf0fc5e32fa7d
Abhinav Anand <abhinavanand072@gmail.com>
2026-01-30 09:18:46 +0530

    Created list styling

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ inputBtn.addEventListener('click', function() {
 function render(myLeads) {
     let listItems = ''
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += `<li><a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a></li>`
+        listItems += `<li><img src="image/link-icon.svg" alt="link-icon"><a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a></li>`
     }
     ulEl.innerHTML = listItems;
 }
